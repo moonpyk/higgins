817a369c9daf5bebbda81fe11db19c940a8bd5f1
Clément Bourgeois <moonpyk@gmail.com>
2014-04-29 00:15:09 +0200

    git init project

---
+++ Higgins.Web/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace Higgins.Web.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
