e7d0a96fc05aabb266fd6bad7994b6eab5e4c6d1
Clément Bourgeois <moonpyk@gmail.com>
2014-04-29 23:16:21 +0200

    Added a Higgins.Selfhost project.

---
+++ Higgins.SelfHost/HigginsStartupSelfHost.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Higgins.Web;
+using Microsoft.Owin.Extensions;
+using Nancy;
+using Nancy.Owin;
+using Owin;
+
+namespace Higgins.SelfHost
+{
+    class HigginsStartupSelfHost
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy(new NancyOptions
+            {
+                Bootstrapper = new HigginsBootstrapper(new MyRootPathProvider())
+            });
+        }
+
+        class MyRootPathProvider : IRootPathProvider
+        {
+            public string GetRootPath()
+            {
+                return Path.GetFullPath(Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory,
+                    "..\\..\\..\\Higgins.Web\\"
+                ));
+            }
+        }
+    }
+}
---
+++ Higgins.SelfHost/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Higgins.Web;
+using Microsoft.Owin.Hosting;
+
+namespace Higgins.SelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new StartOptions
+            {
+                ServerFactory = "Nowin",
+                Port = 8080,
+            };
+
+            using (WebApp.Start<HigginsStartupSelfHost>(options))
+            {
+                Console.WriteLine("Running a http server on port 8080");
+                Console.ReadKey();
+            }
+        }
+    }
+}
